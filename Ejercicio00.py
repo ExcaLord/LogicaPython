4e8c770356c34a51936f3056c1f30ce66b4c1cd3
excalord <hyukaruu@icloud.com>
2025-04-29 13:03:46 -0500

    Ejercicio 00 y Ejercicio 01

---
+++ Ejercicio00.py
@@ -0,0 +1,22 @@
+# Link pagina oficial https://www.python.org/
+"""
+Este es un comentario multilinea de python
+"""
+
+# Comentario de unicamente una linea
+
+# Variable
+name = "Matteo"
+
+# Constante
+# en python no existe la constante, pero se puede simular
+PI = 3.1416
+
+# Tipos de Datos Primitivos
+
+dato_int = 22
+dato_float = 45.23
+dato_str = "Hola Mundo"
+dato_bool = True
+dato_bool2 = False
+print("Hola Python!")
---
+++ Ejercicio01.py
@@ -0,0 +1,83 @@
+# Operadores
+
+# Aritméticos
+
+a = 22
+
+b = 1
+
+# Suma
+print(a + b)
+
+# Resta
+print(a - b)
+
+# Multiplicación
+print(a * b)
+
+# División
+print(a / b)
+
+# Módulo
+print(a % b)
+
+# División entera
+print(a // b)
+
+# Elevado a la potencia
+print(a**b)
+
+# Lógicos
+
+# And
+print(a > 20 and b < 2)
+
+# Or
+print(a > 20 or b < 2)
+
+# Not
+print(not (a > 20 and b < 2))
+
+# Comparación
+
+# Mayor que
+print(a > b)
+
+# Menor que
+print(a < b)
+
+# Mayor o igual que
+print(a >= b)
+
+# Menor o igual que
+print(a <= b)
+
+# Igual que
+print(a == b)
+
+# Diferente de
+print(a != b)
+
+# De asignación
+
+# incremento
+a += 1
+
+# decremento
+a -= 1
+
+# multiplicación
+
+a *= 2
+
+# división
+
+a /= 2
+
+# división entera
+
+a //= 2
+
+# módulo
+
+a %= 2
