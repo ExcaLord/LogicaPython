5364162b3d5c776280fc2f1203b6e3089ee1e8ec
excalord <hyukaruu@icloud.com>
2025-05-15 17:36:34 -0500

    Ejercicio 4
    creacion del archivo del ejercicio 4 de la pagina retos de programacion
    de retos de programacion

---
+++ Ejercicio04.py
@@ -0,0 +1,6 @@
+# Cadenas de Caracteres #
+
+# Existen dos formas de hacer cadenas de texto en Python
+# Una es con comillas dobles u la otra con comillas simples.
+my_str = "Exca Developer is Amazing"
+my_other_str = "Exca Developer is Amazing"
